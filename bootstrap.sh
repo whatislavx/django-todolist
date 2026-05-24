f593e4c83cb4b906de23c99e1684acc32fca384e
Vladyslav Kozlov <kozlov.vlad@lll.kpi.ua>
2026-05-24 21:16:07 +0300

    feat(infra): enhance bootstrap script for Kubernetes setup and ingress controller installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
+
+set -e
+
+kind create cluster --config cluster.yaml || true
+
 kubectl apply -f .infrastructure/mysql/ns.yml
 kubectl apply -f .infrastructure/mysql/configMap.yml
 kubectl apply -f .infrastructure/mysql/secret.yml
 kubectl apply -f .infrastructure/mysql/service.yml
 kubectl apply -f .infrastructure/mysql/statefulSet.yml
+
+kubectl wait --namespace=mysql --for=condition=ready pod/mysql-0 --timeout=90s || true
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=120s
 
 kubectl apply -f .infrastructure/app/ns.yml
 kubectl apply -f .infrastructure/app/pv.yml
@@ -15,6 +29,4 @@ kubectl apply -f .infrastructure/app/nodeport.yml
 kubectl apply -f .infrastructure/app/hpa.yml
 kubectl apply -f .infrastructure/app/deployment.yml
 
-# Install Ingress Controller
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
-# kubectl apply -f .infrastructure/ingress/ingress.yml
+kubectl apply -f .infrastructure/ingress/ingress.yml
